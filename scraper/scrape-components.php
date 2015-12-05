eaa9ff00796a537071363b67d7697b9f3234813d
Nem C <nemc@codeanvil.co>
2015-12-05 04:13:06 +0100

    scraper database import

--- scraper/scrape-components.php
+++ scraper/scrape-components.php
@@ -3,9 +3,27 @@
 require_once 'bootstrap.php';
 
 use Importers\ImportComponents;
+use App\Repository\Entity\Component;
+use App\Repository\Exceptions\QueryException;
+use GuzzleHttp\Exception\ConnectException;
 
 $importer = new ImportComponents();
+try {
+    $components = $importer->doImport();
+} catch (ConnectException $e) {
+    echo $e->getMessage() . PHP_EOL;
+    die;
+}
 
-$components = $importer->doImport();
-var_dump($components);
-die;
+foreach ($components as $c) {
+    try {
+        $component = new Component($c);
+        $component->save();
+    } catch (QueryException $e) {
+        echo $c['name'] . ' skipped because SEPA_ID ' . $c['sepa_id'] . ' most likely already exists' . PHP_EOL;
+        echo $e->getMessage() . PHP_EOL;
+    }
+
+}
+
+echo 'All components saved' . PHP_EOL . PHP_EOL;
--- scraper/scrape-measurements.php
+++ scraper/scrape-measurements.php
@@ -5,14 +5,27 @@ require_once 'bootstrap.php';
 use Importers\ImportAllStations;
 use Importers\ImportComponents;
 use Importers\ImportMeasurements;
+use App\Repository\Entity\Measurement;
+use App\Repository\Exceptions\QueryException;
+use GuzzleHttp\Exception\ConnectException;
+use App\Repository\StationRepository;
+use App\Repository\ComponentRepository;
 
 $stationsImporter = new ImportAllStations();
-$stations = $stationsImporter->doImport();
+try {
+    $stations = $stationsImporter->doImport();
+} catch (ConnectException $e) {
+    echo $e->getMessage() . PHP_EOL;
+    die;
+}
 
 $componentsImporter = new ImportComponents();
-$components = $componentsImporter->doImport();
-
-$measurements = [];
+try {
+    $components = $componentsImporter->doImport();
+} catch (ConnectException $e) {
+    echo $e->getMessage() . PHP_EOL;
+    die;
+}
 
 foreach ($stations as $key => $station) {
     if (isset($measurements[$station['sepa_id']]) === false) {
@@ -20,6 +33,37 @@ foreach ($stations as $key => $station) {
     }
 
     $measurementImporter = new ImportMeasurements($station['sepa_id'], $components);
-    $measurements[$station['sepa_id']] = $measurementImporter->doImport();
+    try {
+        $measurements = $measurementImporter->doImport();
+    } catch (ConnectException $e) {
+        echo $e->getMessage() . PHP_EOL;
+        die;
+    }
+
+    $stationRepo = StationRepository::getInstance();
+    $componentRepo = ComponentRepository::getInstance();
+
+    $station = $stationRepo->findBySepaId($station['sepa_id']);
+
+    foreach ($measurements as $hourly) {
+
+        $timestamp = $hourly['timestamp'];
+
+        foreach ($hourly['com_values'] as $componentSepaId => $value) {
+            if (is_null($value) === false) {
+                $component = $componentRepo->findBySepaId($componentSepaId);
+                $measurement = new Measurement([
+                    'station_id' => $station->getId(),
+                    'measure_timestamp' => $timestamp,
+                    'component_id' => $component->getId(),
+                    'alert' => 0,
+                ]);
+                $measurement->save();
+            }
+        }
+
+
+    }
 }
-die;
+
+echo 'All measurements saved' . PHP_EOL . PHP_EOL;
